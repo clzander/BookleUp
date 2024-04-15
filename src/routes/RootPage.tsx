3a3c8cd70cfc1c80389bd8e8243012cffb30a583
= <cle.zander@gmail.com>
2024-04-15 13:01:49 +0200

    Added a first navbar

--- src/routes/RootPage.tsx
+++ src/routes/RootPage.tsx
@@ -1,7 +1,7 @@
+import Navbar from "../components/Navbar";
+
 export default function RootPage() {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <Navbar />
     )
 }
