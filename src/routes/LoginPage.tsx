4f81dc4550b46c46b937db8fa4cd9dbedd4a3f7a
= <cle.zander@gmail.com>
2024-05-13 11:53:52 +0200

    Users can now go back and forth between login and signup page

--- src/routes/LoginPage.tsx
+++ src/routes/LoginPage.tsx
@@ -1,4 +1,8 @@
+import { useNavigate } from "react-router"
+
 export default function LoginPage() {
+    const navigate = useNavigate()
+
     return (
         <div className="relative flex flex-col justify-center h-screen overflow-hidden">
             <div className="w-full p-6 m-auto bg-base-200 rounded-md shadow-md ring-2 ring-gray-800/50 lg:max-w-lg">
@@ -17,10 +21,11 @@ export default function LoginPage() {
                         <input type="password" placeholder="Enter Password"
                             className="w-full input input-bordered" />
                     </div>
-                    <a href="#" className="text-xs hover:underline hover:text-blue-600">Forget Password?</a>
+                    <a href="" className="text-xs hover:underline hover:text-blue-600">Forget Password?</a>
                     <div>
                         <button className="btn btn-block bg-base-200">Login</button>
                     </div>
+                    <a onClick={() => navigate("/signup")} className="text-xs hover:underline hover:cursor-pointer hover:text-blue-600">Don't have an account yet? Sign up!</a>
                 </form>
             </div>
         </div>
